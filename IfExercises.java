7c41dc2d32e39de64eefc6801fa43400cc7f293b
Giulia Montechiarello <150006058+giuliamonte21@users.noreply.github.com>
2025-10-16 20:23:02 +0200

    Add files via upload
    Questo codice riporta il seguente errore:
    
    Error: Could not find or load main class IfExercises
    Caused by: java.lang.ClassNotFoundException: IfExercises

---
+++ IfExercises.java
@@ -0,0 +1,17 @@
+public class IfExercises {
+
+    public static void main(String[] args) {
+        boolean isOnline = true;
+        String nome = "Luca";
+
+        if (nome.equals("Luca")) {
+            System.out.println("è Luca");
+        } else if (nome.equals("Marco")) {
+            System.out.println("è Marco");
+        } else {
+            System.out.println("è qualcun altro");
+        }
+    }
+}
+
+//RIGUARDARE: NON FUNZIONA
